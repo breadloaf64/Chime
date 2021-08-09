199784a40116125bb39807ec7ae4e068abfbdea8
Peter Marsh <breadloaf64@gmail.com>
2021-08-09 22:58:16 +0100

    added screenshake component

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour
-{
-    float shakeMagnitude;
+public class CameraShake : MonoBehaviour {
+    [SerializeField] float period;
     [SerializeField] float damping;
+    [SerializeField] float strength;
+    [SerializeField] float maxMagnitude;
+
+    float magnitude;
+    int counter;
+
+    Vector3 posPrev;
+    Vector3 posNext;
 
     Vector3 equilibrium;
 
@@ -13,25 +20,41 @@ public class CameraShake : MonoBehaviour
     void Start()
     {
         equilibrium = transform.position;
+        posPrev = equilibrium;
+        posNext = equilibrium;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Shake();
-        shakeMagnitude *= (1 - damping);
+        if (magnitude > 0.001) {
+            HandleShake();
+        }
     }
 
-    void Shake() {
+    void HandleShake() {
+        counter++;
+
+        if (counter >= period) {
+            counter = 0;
+            posPrev = posNext;
+            posNext = NewShakePoint();
+            magnitude *= (1 - damping);
+        }
+
+        transform.position = Vector3.Lerp(posPrev, posNext, (float)counter / period);
+    }
+
+    Vector3 NewShakePoint() {
         float theta = Random.Range(0, 2 * Mathf.PI);
-        float x = shakeMagnitude * Mathf.Cos(theta);
-        float y = shakeMagnitude * Mathf.Sin(theta);
+        float x = magnitude * Mathf.Cos(theta);
+        float y = magnitude * Mathf.Sin(theta);
         Vector3 offset = new Vector3(x, y, 0);
-
-        this.transform.position = equilibrium + offset;
+        return offset + equilibrium;
     }
 
-    public void AddShake(float magnitude) {
-        shakeMagnitude += magnitude * 0.02f;
+    public void AddShake(float addMagnitude) {
+        magnitude += addMagnitude * strength;
+        magnitude = Mathf.Clamp(magnitude, 0, maxMagnitude);
     }
 }
