65375f3eb2d47c9e11b4e442806f72084f54b3b4
Peter Marsh <breadloaf64@gmail.com>
2021-08-18 23:13:44 +0100

    Created confirmation dialog, which pops up when delete level button is pressed

--- Assets/Scripts/UI Scripts/Button Scripts/ButtonHandler_FromFile.cs
+++ Assets/Scripts/UI Scripts/Button Scripts/ButtonHandler_FromFile.cs
@@ -14,4 +14,31 @@ public class ButtonHandler_FromFile : BtnHandler {
         LevelSaveLoad.DeleteUserLevel(level);
         ulp.nextIndex();
     }
+
+    public void TriggerDeleteLevelWithConfirmation() {
+        StartCoroutine(DeleteLevelWithConfirmation());
+
+    }
+
+    public IEnumerator DeleteLevelWithConfirmation() {
+        UserLevelPanel ulp = FindObjectOfType<UserLevelPanel>();
+        string level = ulp.SelectedLevel();
+
+        Dialog_Confirmation dialog = FindObjectOfType<Dialog_Confirmation>();
+        dialog.Show("Are you sure you want to delete " + level + "?");
+
+        while (dialog.result == Dialog_Confirmation.Result.None) {
+            yield return null; // wait
+        }
+
+        if (dialog.result == Dialog_Confirmation.Result.Yes) {
+            LevelSaveLoad.DeleteUserLevel(level);
+            ulp.nextIndex();
+            Debug.Log("result was yes");
+        }
+        else if (dialog.result == Dialog_Confirmation.Result.No) {
+            // do nothing
+            Debug.Log("result was No");
+        }
+    }
 }
