eeb883c76f0fd10bf330e1eed4beb183d34480a3
Peter Marsh <breadloaf64@gmail.com>
2021-08-25 22:41:04 +0100

    can now use tab buttons to swap between tabs

--- Assets/Scripts/UI Scripts/PullDown.cs
+++ Assets/Scripts/UI Scripts/PullDown.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PullDown : MonoBehaviour
-{
+public class PullDown : MonoBehaviour {
     [SerializeField] bool state; // false = up, true = down
     [SerializeField] Animator animator;
     [SerializeField] TextMesh toggleText;
+    [SerializeField] GameObject[] tabs;
 
     public void Toggle() {
         if (state) {
@@ -22,4 +22,17 @@ public class PullDown : MonoBehaviour
     }
 
     public bool GetState() { return state; }
+
+    public void ChangeTab(int tabIndex) {
+        if (tabIndex < 0 || tabIndex >= tabs.Length) {
+            Debug.LogWarning("There is no tab with index " + tabIndex);
+            
+        }
+        else {
+            foreach(GameObject tab in tabs) {
+                tab.SetActive(false);
+            }
+            tabs[tabIndex].SetActive(true);
+        }
+    }
 }
